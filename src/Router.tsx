5a325523d19e2069861162bf0f01f6b2bea38169
Sunny <heysunny612@gmail.com>
2023-07-02 14:16:04 +0900

    Add: 페이지 구성 및 Router 설정

---
+++ src/Router.tsx
@@ -0,0 +1,26 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import App from './App';
+import NotFound from './pages/NotFound';
+import Home from './pages/Home';
+import About from './pages/About';
+import Portfolio from './pages/Portfolio';
+import Blog from './pages/Blog';
+import Contact from './pages/Contact';
+
+export default function Router() {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <App />,
+      errorElement: <NotFound />,
+      children: [
+        { index: true, element: <Home /> },
+        { path: 'about', element: <About /> },
+        { path: 'portfolio', element: <Portfolio /> },
+        { path: 'blog', element: <Blog /> },
+        { path: 'contact', element: <Contact /> },
+      ],
+    },
+  ]);
+  return <RouterProvider router={router} />;
+}
---
+++ src/pages/Home.tsx
@@ -0,0 +1,3 @@
+export default function Home() {
+  return <main>홈입니다</main>;
+}
